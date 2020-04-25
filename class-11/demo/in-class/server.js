5e9cfbb57d1a6b1fdc7ec6cf8ba4f37d5a203179
John Cokos <highlander.44@gmail.com>
2020-04-25 16:29:46 -0700

    Added some book searching

--- class-11/demo/in-class/server.js
+++ class-11/demo/in-class/server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -30,17 +30,53 @@ app.get('/person', (request, response) => {
 
   console.log(data);
 
-  response.status(200).render('person.ejs', {person:data});
+  response.status(200).render('pages/person.ejs', {person:data});
 });
 
 app.post('/article', (request, response) => {
-  response.status(200).render('article', {article: request.body.content});
+  response.status(200).render('pages/article', {article: request.body.content});
 });
 
 // This will force an error
 app.get('/badthing', (request,response) => {
   throw new Error('WTF???');
 });
+
+// ------------ IT's ABOUT TO GET REAL ---------------- //
+
+app.get('/searchForm', (request, response) => {
+  response.status(200).render('pages/search-form');
+});
+
+app.post('/search', (request, response) => {
+
+  // https://developers.google.com/books/docs/v1/using
+  // request.body (came form POST) { search: 'hockey', searchby: 'intitle' }
+  // ?q=intitle:hockey
+  // request.body (came form POST) { search: 'kronika', searchby: 'inauthor' }
+  // ?q=inauthor:kronika
+
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  let queryObject = {
+    q: `${request.body.searchby}:${request.body.search}`,
+  };
+
+  superagent.get(url)
+    .query( queryObject )
+    .then( results => {
+      // our books are in results.body ...
+      let books = results.body.items.map( book => new Book(book) );
+      response.status(200).render('pages/search-results', {books: books});
+    });
+
+});
+
+function Book(data) {
+  this.title = data.volumeInfo.title;
+  this.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount : 'Unknown';
+}
+
+// ------------ /IT's ABOUT TO GET REAL ---------------- //
 
 // 404 Handler
 app.use('*', (request, response) => {
